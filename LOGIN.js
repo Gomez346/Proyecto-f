ec6fb1cec0a4be91cb8daa7335f0fab54488d694
Gomez346 <gaby64838342@gmail.com>
2023-12-04 18:12:28 -0400

    cambio de posicion del boton

--- LOGIN.js
+++ LOGIN.js
@@ -1,8 +1,14 @@
-function validateForm() {
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
-    if (username === 'aby' || password === '1234') {
-      alert('Por favor, ingrese nombre de usuario y contraseña');
-      return false;
-    }
+function validarCredenciales() {
+  // Obtener los valores de usuario y contraseña
+  var usuario = document.getElementById('usuario').value;
+  var contrasena = document.getElementById('contrasena').value;
+
+  // Verificar si los valores son válidos
+  if (usuario === 'usuario_valido' && contrasena === 'contrasena_valida') {
+      // Redireccionar a otra página
+      window.location.href = 'DashBoard.html';
+  } else {
+      // Mostrar un mensaje de error
+      alert('Usuario o contraseña incorrectos');
   }
+}
